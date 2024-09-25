c1aabaf42e93803b8569e94805beaad5db2e4bcc
mayurgajbhiye4 <mayurgajbhiye4@gmail.com>
2024-09-26 00:53:44 +0530

    login function in js using fetch

--- base/views_api.py
+++ base/views_api.py
@@ -29,6 +29,7 @@ class LoginView(APIView):
             user_obj = authenticate(username = data.get('username'), password = data.get('password'))
 
             if user_obj:
+                login(request, user_obj)
                 response['status'] = 200
                 response['message'] = 'Welcome'
             else:
